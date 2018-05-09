82d61ba69bfeb4778d3f654f5782ca22d7bda6c7
Yang, Lingzhen <lingzhen.yang@sap.com>
2018-05-09 15:52:14 +0800

    use css module

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import messages from './messages.json';
 
+import styles from './Greeter.css';
+
 class Greeter extends Component {
     render() {
         return (
-            <div>
+            <div className={styles.message}>
                 {messages.greetingText}
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ module.exports = {
     devtool: 'eval-source-map',   // set source mapping to enhance the debugging process
     entry:  __dirname + "/index.js",
     output: {
-      path: __dirname + "/public",//打包后的文件存放的地方
-      filename: "bundle.js"//打包后输出文件的文件名
+      path: __dirname + "/public", //打包后的文件存放的地方
+      filename: "bundle.js" //打包后输出文件的文件名
     },
     devServer: {
-      contentBase: "./public",//本地服务器所加载的页面所在的目录
-      historyApiFallback: true,//不跳转
-      inline: true//实时刷新
+      contentBase: "./public", //本地服务器所加载的页面所在的目录
+      historyApiFallback: true, //不跳转
+      inline: true //实时刷新
     },
     module: {
         rules: [
@@ -25,7 +25,11 @@ module.exports = {
                     {
                         loader: "style-loader"
                     }, {
-                        loader: "css-loader"
+                        loader: "css-loader",
+                        options: {
+                            modules: true, // launch css modules
+                            localIdentName: '[name]__[local]--[hash:base64:5]' // set class name format
+                        }
                     }
                 ]
             }
