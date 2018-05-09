e080ff30e1c9e1be979a20e4c669ef64de40902c
Yang, Lingzhen <lingzhen.yang@sap.com>
2018-05-09 11:09:21 +0800

    set source mapping and set up webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+    devtool: 'eval-source-map',   // set source mapping to enhance the debugging process
     entry:  __dirname + "/index.js",
     output: {
       path: __dirname + "/public",//打包后的文件存放的地方
       filename: "bundle.js"//打包后输出文件的文件名
-    }
+    },
+    devServer: {
+      contentBase: "./public",//本地服务器所加载的页面所在的目录
+      historyApiFallback: true,//不跳转
+      inline: true//实时刷新
+    } 
+
 }
